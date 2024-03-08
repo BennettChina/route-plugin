27e7fe902b69f750c868f9ec893630fa236f9a7f
bennett <w2500513053@outlook.com>
2024-03-08 21:52:37 +0800

    fix key bug.

--- routes/help.ts
+++ routes/help.ts
@@ -2,7 +2,7 @@ import express from "express";
 import bot from "ROOT";
 
 export default express.Router().get( "/", async ( req, res ) => {
-	const data = await bot.redis.getString( "route-plugin:help" );
+	const data = await bot.redis.getString( "route-plugin.help" );
 	if ( !data ) {
 		return res.status( 404 ).send( "Not Found" );
 	}
